2066a3cce914449719bd94a3d298e473fbd2363e
BaselMoqbel <100458824+BaselMoqbel@users.noreply.github.com>
2022-08-23 17:40:50 -0500

    add prework files to repository

---
+++ preworkapp/app/src/main/java/com/example/pre_workapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.pre_workapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //where to write the logic
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            //handle button tap
+            Log.i("Basel","tapped on button")
+            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.white))
+
+
+        }
+    }
+}
